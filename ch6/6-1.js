236e9740ac054ece4befc5c9a89799d14a8ee134
you-eun-hye <[dkfvktorcp@naver.com]>
2024-10-23 15:15:41 +0900

    6.3 함수 추출하기

--- ch6/6-1.js
+++ ch6/6-1.js
@@ -1,24 +1,39 @@
+// 너무 길었던 함수를 실행되는 순서대로 함수를 나누어 가독성 높이기
 export function printOwing(invoice) {
-  let outstanding = 0;
+  printBanner();
+  let outstanding = calculateOutstanding(invoice);
+  recordDueDate(invoice);
+  printDetails(invoice, outstanding);
+}
 
+function printBanner(){
   console.log('***********************');
   console.log('**** Customer Owes ****');
   console.log('***********************');
+}
 
-  // calculate outstanding
-  for (const o of invoice.orders) {
-    outstanding += o.amount;
-  }
+function calculateOutstanding(invoice){
+// 절차적 프로그래밍
+//  let result = 0;
+//  for (const o of invoice.orders) {
+//    result += o.amount;
+//  }
+//  return result;
 
-  // record due date
+// 함수형 프로그래밍
+return invoice.orders.reduce((sum, order) => (sum += order.amount), 0);
+}
+
+function recordDueDate(invoice) {
   const today = new Date();
   invoice.dueDate = new Date(
     today.getFullYear(),
     today.getMonth(),
     today.getDate() + 30
   );
+}
 
-  //print details
+function printDetails(invoice, outstanding) {
   console.log(`name: ${invoice.customer}`);
   console.log(`amount: ${outstanding}`);
   console.log(`due: ${invoice.dueDate.toLocaleDateString()}`);
