48e4aeadb458b7865bcecf86aa429515f8cfeb18
you-eun-hye <[dkfvktorcp@naver.com]>
2024-10-27 22:59:59 +0900

    CH6 과제

--- ch6/6-11.js
+++ ch6/6-11.js
@@ -1,30 +1,45 @@
 export function priceOrder(product, quantity, shippingMethod) {
-  const basePrice = product.basePrice * quantity;
-  const discount =
-    Math.max(quantity - product.discountThreshold, 0) *
-    product.basePrice *
-    product.discountRate;
-  const shippingPerCase =
-    basePrice > shippingMethod.discountThreshold
-      ? shippingMethod.discountedFee
-      : shippingMethod.feePerCase;
-  const shippingCost = quantity * shippingPerCase;
-  const price = basePrice - discount + shippingCost;
-  return price;
+    const basePrice = product.basePrice * quantity;
+    const discount = calculateDiscountedPrice(product, quantity);
+    const shippingCost = calculateShippingCost(
+        basePrice, 
+        quantity, 
+        shippingMethod
+    );
+
+    return basePrice - discount + shippingCost;
 }
 
+function calculateDiscountedPrice(product, quantity) {
+    return (
+        Math.max(quantity - product.discountThreshold, 0) *
+        product.basePrice *
+        product.discountRate
+    );
+}
+
+function calculateShippingCost(basePrice, quantity, shippingMethod) {
+    const shippingPerCase = 
+      basePrice > shippingMethod.discountThreshold
+        ? shippingMethod.discountedFee
+        : shippingMethod.feePerCase;
+
+    return quantity * shippingPerCase;
+}
+  
 // 사용 예:
 const product = {
-  basePrice: 10,
-  discountRate: 0.1,
-  discountThreshold: 10,
+    basePrice: 10,
+    discountRate: 0.1,
+    discountThreshold: 10,
 };
 
 const shippingMethod = {
-  discountThreshold: 20,
-  feePerCase: 5,
-  discountedFee: 3,
+    discountThreshold: 20,
+    feePerCase: 5,
+    discountedFee: 3,
 };
 
 const price = priceOrder(product, 5, shippingMethod);
 console.log(price);
+  
